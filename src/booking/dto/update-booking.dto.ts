72ff67b9f4e1533d7ecbb270c41930287fa68e34
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-08-08 13:10:52 +0100

    refactor: update UpdateOrderDTO

--- src/booking/dto/update-booking.dto.ts
+++ src/booking/dto/update-booking.dto.ts
@@ -1,9 +1,42 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBookingDto } from './create-booking.dto';
-import { IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { OrderStatus } from '../schemas/booking.schema';
 
 export class UpdateBookingDto extends PartialType(CreateBookingDto) {
   @IsString()
   @IsNotEmpty()
-  status: string;
+  orderStatus: OrderStatus;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  orderId: string;
+
+  @IsOptional()
+  orderStartCoord?: any;
+
+  @IsOptional()
+  riderPickUpCoord?: any;
+
+  @IsNumber()
+  @IsOptional()
+  deliveryCode?: number;
+
+  @IsOptional()
+  riderDropOffCoord?: any;
+
+  @IsOptional()
+  @IsString()
+  deliveryImageLink?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isSdkDemo?: boolean;
 }
