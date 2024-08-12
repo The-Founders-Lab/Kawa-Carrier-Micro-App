bcbfeb8731c380cca255bec89d6f697c750fc7c9
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-08-12 15:47:46 +0100

    refactor: move update-status logic to service

--- api/src/orders/orders.service.ts
+++ api/src/orders/orders.service.ts
@@ -2,7 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { Order } from './schemas/orders.schema';
+import { Order, OrderStatusEnum } from './schemas/orders.schema';
 import { Model } from 'mongoose';
 import { RidersService } from 'src/riders/riders.service';
 import { RiderStatusEnum } from 'src/riders/schemas/riders.schema';
@@ -81,6 +81,66 @@ export class OrdersService {
 
   findOne(orderId: string) {
     return this.orderModel.findOne({ orderId });
+  }
+
+  async updateStatus(update: UpdateOrderDto) {
+    if (!Object.values(OrderStatusEnum).includes(update.orderStatus)) {
+      throw new BadRequestException('Invalid status update');
+    }
+
+    const { orderId } = update;
+    const order = await this.findOne(orderId);
+    if (!order) {
+      throw new BadRequestException('Order not found');
+    }
+
+    try {
+      const resp = await fetch(
+        `${config.KAWA_SDK_BASE_URL}/orders/update-order-status`,
+        {
+          body: JSON.stringify({
+            ...update,
+            orderId,
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${config.KAWA_INTEGRATION_KEY}`,
+          },
+          method: 'POST',
+        },
+      );
+      const respJson = await resp.json();
+      console.log('req', {
+        ...update,
+        orderId,
+      });
+      console.log('resp', respJson);
+      console.log();
+
+      if (respJson.statusCode === 200) {
+        await this.update(order.id, {
+          data: {
+            ...order.data,
+            orderStatus: update.orderStatus,
+          },
+        });
+      }
+
+      if (
+        respJson.statusCode === 200 &&
+        update.orderStatus === OrderStatusEnum.delivered
+      ) {
+        await this.ridersService.updateRiderStatus(
+          order.data?.rider,
+          RiderStatusEnum.Available,
+        );
+      }
+
+      return { message: respJson?.message || 'updated', data: respJson };
+    } catch (error) {
+      console.log('Error updating order', error);
+      return error;
+    }
   }
 
   update(orderId: string, update: Partial<UpdateOrderDto>) {
