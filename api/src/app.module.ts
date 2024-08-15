406fe6a96fe384b8a5a9dec10a19ffeea1fb316b
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-08-15 11:39:22 +0100

    refactor: change hard values to use environment variables

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -6,16 +6,17 @@ import { OrdersModule } from './orders/orders.module';
 import { WebhookModule } from './webhook/webhook.module';
 import { RidersModule } from './riders/riders.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import config from './config';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '',
-      database: 'microapp',
+      host: config.DB_HOST,
+      port: parseInt(config.DB_PORT) || 5432,
+      username: config.DB_USERNAME,
+      password: config.DB_PASSWORD,
+      database: config.DB_DATABASE_NAME,
       autoLoadEntities: true,
       synchronize: true, // should not be used in production. It could result to loss of data
     }),
--- api/src/config/index.ts
+++ api/src/config/index.ts
@@ -1,5 +1,12 @@
 export default {
-  MONGO_URI: process.env.MONGO_URI,
+  // db config
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_DATABASE_NAME: process.env.DB_DATABASE_NAME,
+
+  // integration config
   KAWA_INTEGRATION_KEY: process.env.KAWA_INTEGRATION_KEY,
   KAWA_SDK_BASE_URL: process.env.KAWA_SDK_BASE_URL,
 };
