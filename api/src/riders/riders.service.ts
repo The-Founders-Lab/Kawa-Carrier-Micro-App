2cb8474a2f202e40fc3343f679f4412d063890fa
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-09-10 15:32:13 +0100

    feat: add functionality for create new rider

--- api/src/riders/riders.service.ts
+++ api/src/riders/riders.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Rider } from './rider.entity';
 import { Repository } from 'typeorm';
+import { faker } from '@faker-js/faker';
+import { CreateRiderDto } from './dtos/create-rider.dto';
 
 @Injectable()
 export class RidersService {
@@ -9,7 +11,22 @@ export class RidersService {
     @InjectRepository(Rider) private ridersRepository: Repository<Rider>,
   ) {}
 
-  getRiders() {
+  createRider() {
+    const newRider = this.ridersRepository.create({
+      riderImage: '',
+      riderFirstName: faker.person.firstName(),
+      riderLastName: faker.person.lastName(),
+      riderPhone: faker.phone.number({ style: 'human' }),
+      vehiclePlate: faker.vehicle.vin(),
+      vehicleBrand: faker.vehicle.vehicle(),
+      vehicleModel: faker.vehicle.model(),
+      color: faker.vehicle.color(),
+      vehicleType: faker.vehicle.type(),
+    });
+    return this.ridersRepository.save(newRider);
+  }
+
+  async getRiders() {
     return this.ridersRepository.find();
   }
 
